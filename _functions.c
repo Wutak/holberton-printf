f5db20c1f5c86787b1c8466e4a246b7e11efbc7d
root <pertonsalomee@gmail.com>
2024-11-26 13:58:18 +0000

    _functions.c

--- _functions.c
+++ _functions.c
@@ -16,16 +16,42 @@ void _putstr(char *s)
 	}
 }
 
+/**
+ * _putchar - writes a single char to the output
+ * @c: char to be printed
+ * Return: nothing
+ */
+
 void _putchar(char c)
 {
 	write(1, &c, 1);
 }
+
+/**
+ * _putnbr - writes an int to the output
+ * @n: the int to be printed
+ * Return: nothing
+ */
 
 void _putnbr(int n)
 {
 	int i = 0;
 
 	if (n == -2147483648)
-		return ("Error");
-	
-	while (i < n
+	{
+		write(1, "-2147483648", 11);
+		return;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		_putnbr(n / 10);
+	}
+
+	i = (n % 10) + '0';
+	_putchar(i);
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void _putstr(char *s);
+void _putnbr(int n);
+int _printf(const char *format, ...);
+
+#endif
