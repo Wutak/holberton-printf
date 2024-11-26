3066d7ac5e303afda4ce5d48b24ab81a7ba52277
julien <julienvillessot@gmail.com>
2024-11-26 13:17:04 +0000

    putnbr finit

--- _functions.c
+++ _functions.c
@@ -23,9 +23,15 @@ void _putchar(char c)
 
 void _putnbr(int n)
 {
-	int i = 0;
-
 	if (n == -2147483648)
 		return ("Error");
 	
-	while (i < n
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n  >= 10)
+		_putnbr(n / 10);
+	_putchar(n % 10 = '0');
+}
--- _printf.c
+++ _printf.c
@@ -1,6 +1,13 @@
 #include <stdarg.h>
 #include <unistd.h>
-
+int get_function(va_list args, const char *format)
+{
+	if (format[1] == 's')
+		return (_putstr(va_args(args, char *)));
+	if (format[1] == 'c')
+		return (_putchar(va_args(args char *)));
+	if (format[1] == '%')
+}
 /**
  * _printf - will look through the string for the % first, then the option, or straight for the args
  * @format: string of args
@@ -13,7 +20,7 @@ int _printf(const char *format, ...)
 	int len = 0;
 
 	va_start(args, format);
-	while(format[++i]) // permet d'eviter une incrementation qui traine et qui pourrait faire defaut si jamais oubli√e.
+	while(format[++i]) // permet d'eviter une incrementation qui traine et qui pourrait faire defaut en cas d'oubli√.
 	{
 		if (format[i] == '%' et fonction2(format[i + 1]))
 			len += format_string(args, format + i++);
