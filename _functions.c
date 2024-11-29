c007d16fb5129b9b42a992b34719aa5eabe6f75a
Wutak <julienvillessot@gmail.com>
2024-11-29 12:21:46 +0100

    Update _functions.c putnbr, len

--- _functions.c
+++ _functions.c
@@ -35,19 +35,23 @@ int _putchar(char c)
  */
 int _putnbr(int n)
 {
+	int len = 0;
+	
 	if (n == -2147483648)
 	{
 		write(1, "-2147483648", 11);
-		return (0);
+		return (11);
 	}
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
+		len++;
 	}
 	if (n  >= 10)
+		len++;
 		_putnbr(n / 10);
 
 	_putchar(n % 10 + '0');
-	return (0);
+	return (len);
 }
